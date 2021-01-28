6086a2fcbd2845e94cc37c06a6b7bc5e0c6737cf
a.nurmukhambetov <a.nurmukhambetov@ensoft.kz>
2021-01-28 19:58:04 +0600

    Concrete View Classes

--- ebooksapi/ebooks/api/urls.py
+++ ebooksapi/ebooks/api/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from ebooks.api.views import EbookListCreateAPIView
+from ebooks.api.views import (ReviewCreateAPIView, ReviewDetailAPIView,
+                              EbookListCreateAPIView, EbookDetailAPIView)
 
 urlpatterns = [
-  path("ebooks/", EbookListCreateAPIView.as_view(), name="ebooks-list")
+  path("ebooks/", EbookListCreateAPIView.as_view(), name="ebooks-list"),
+  path("ebooks/<int:pk>/", EbookDetailAPIView.as_view(), name="ebooks-detail"),
+  path("ebooks/<int:ebook_pk>/review/", ReviewCreateAPIView.as_view(), name="ebook-review"),
+  path("reviews/<int:pk>/", ReviewDetailAPIView.as_view(), name="review-detail"),
 ]
